8e81edf76fd6e9892cfadbdf92ed6b46fd392cbc
rodrigo15975 <javierluis15975@hotmail.com>
2025-08-28 07:23:42 -0500

    update auth.service.ts

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { proxyAxiosMethods } from '@/common/adapters/adapters'
 import { PathServices } from '@/common/path-services/path.services'
-import { AuthResponse, Login, Signup } from '../types/auth'
 import { ResponseServer } from '@/helpers/response-server'
+import { AuthResponse, Signup } from '../types/auth'
 
 export const signup = async (data: Signup) =>
   await proxyAxiosMethods.POST<ResponseServer<AuthResponse>, Signup>(
